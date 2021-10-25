e95943ac25d33c5a0d84ab028a7e888d81af3dc7
maddog <maddog1224@outlook.com>
2021-10-24 23:06:38 -0400

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,17 +7,39 @@ public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
 
-    public float speed;
-
-    public Text score;
-
+    public float speed = 0;
+    public Text LivesText;
+    public Text WinText;
+    public Text ScoreText;
+    public float jumpForce;
+    
     private int scoreValue = 0;
+    private int lives;
+    private int count;
+    private int score;
 
+    public AudioClip musicClipTwo;
+    public AudioSource musicSource;
+    private bool facingRight = true;
+    public float hozMovement;
+    private bool isOnGround;
+    public Transform groundcheck;
+    public float checkRadius;
+    public LayerMask allGround;
+   
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        ScoreText.text = scoreValue.ToString();
+
+
+        
+            WinText.text = "";
+            lives = 3;
+            SetLivesText();
+        
+
     }
 
     // Update is called once per frame
@@ -26,33 +48,133 @@ public class PlayerScript : MonoBehaviour
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
-    }
+        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
 
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+
+        }
+
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+
+        }
+
+    }
+    void Update()
+    {
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
+
+        if (scoreValue >= 1)
+        {
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+        }
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            ScoreText.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+            SetScoreText();
+
+            if (scoreValue == 4)
+            {
+                transform.position = new Vector3(60.0f, 0.2f, 5.0f);
+            }
         }
+
+        if (collision.collider.tag == "Enemy")
+        {
+            lives -= 1;
+            LivesText.text = lives.ToString();
+            Destroy(collision.collider.gameObject);
+            SetLivesText();
+        }
+
 
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Ground")
+        if (collision.collider.tag == "Ground" && isOnGround)
         {
             if (Input.GetKey(KeyCode.W))
             {
-                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                rd2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             }
         }
 
 
-        if (Input.GetKey("escape"))
+
+
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+
+
+       
+            if (other.gameObject.CompareTag("Coin"))
+            {
+                other.gameObject.SetActive(false);
+                score = score + 1;
+                SetScoreText();
+            }
+
+
+            else if (other.gameObject.CompareTag("Enemy"))
+            {
+                other.gameObject.SetActive(false);
+                lives = lives - 1;
+                SetLivesText();
+            }
+
+
+
+    }
+
+    void SetScoreText()
+    {
+        ScoreText.text = "score " + scoreValue.ToString();
+        if (scoreValue >= 8)
         {
-            Application.Quit();
+            WinText.text = "You Win! Game created by James Draper";
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+            Destroy(gameObject);
         }
     }
+
+    void SetLivesText()
+    {
+        LivesText.text = "lives " + lives.ToString();
+        if (lives <= 0)
+        {
+            WinText.text = "You Lose :(";
+            Destroy(gameObject);
+        }
+    }
+
+ 
+
+
 }
